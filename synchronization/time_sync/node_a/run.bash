688b181c613af8f0058addd79f56f7e997fa1963
IrfanHakimm <mirfanhakim08@gmail.com>
2025-11-16 01:45:42 +0700

    fix: change node d ip address

--- synchronization/time_sync/node_a/run.bash
+++ synchronization/time_sync/node_a/run.bash
@@ -2,7 +2,7 @@
 
 python3 peer_node.py \
 	  --name A --listen 0.0.0.0 5000 \
-	  --peers A@192.168.122.159:5000 B@192.168.122.198:5001 D@192.168.122.253:5002 \
+	  --peers A@192.168.122.159:5000 B@192.168.122.198:5001 D@192.168.122.37:5002 \
 	  --logger 192.168.122.253 9999 \
 	  --offset-ms 600 \
 	  --initiate-broadcast --msg "Hello from A"
--- synchronization/time_sync/node_c/run.bash
+++ synchronization/time_sync/node_c/run.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 logger.py --bind 0.0.0.0 --port 9999 --expect 10
+python3 logger.py --bind 0.0.0.0 --port 9999 --expect 20
