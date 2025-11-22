387c6dbd02d8f62684ad9136a26e33f04170aeeb
IrfanHakimm <mirfanhakim08@gmail.com>
2025-11-22 22:19:32 +0700

    chore: update testing script to using vm ip

--- synchronization/mutex/testing/mutex.sh
+++ synchronization/mutex/testing/mutex.sh
@@ -1,11 +1,11 @@
 # Race two writers (great for no-mutex demo)
-python3 ./kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003,127.0.0.1:8004 race "PUT color black" "PUT color white"
+python3 ./kvclient.py --nodes 192.168.122.159:8001,192.168.122.198:8002,192.168.122.253:8003,192.168.122.37:8004 race "PUT color black" "PUT color white"
 
 # Read the key from ALL nodes after the race
-python3 ./kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003,127.0.0.1:8004 getall color
+python3 ./kvclient.py --nodes 192.168.122.159:8001,192.168.122.198:8002,192.168.122.253:8003,192.168.122.37:8004 getall color
 
 # Race two writers (great for no-mutex demo)
-python3 ./kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003,127.0.0.1:8004 race "PUT color red" "PUT color blue"
+python3 ./kvclient.py --nodes 192.168.122.159:8001,192.168.122.198:8002,192.168.122.253:8003,192.168.122.37:8004 race "PUT color red" "PUT color blue"
 
 # Read the key from ALL nodes after the race
-python3 ./kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003,127.0.0.1:8004 getall color
+python3 ./kvclient.py --nodes 192.168.122.159:8001,192.168.122.198:8002,192.168.122.253:8003,192.168.122.37:8004 getall color
